f8e9f334d2223cc2f35d0e457ceb41c76dad73e9
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-06-13 00:48:43 +0600

    home page done

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="col-md-6 my-5 py-5 px-5">
+                <h1 className="text-center text-white py-5">Monitor the Air Quality parameters at various locations in
+                London</h1>
+            </div>
+           
+        </div>
+    );
+};
+
+export default Home;
